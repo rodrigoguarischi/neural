df42918d39f351f3102e691bac921d53c6c3b1a2
Rodrigo Guarischi-Sousa <rodrigoguarischi@gmail.com>
2021-02-14 20:36:32 -0300

    App working end to end. Adding some code comments

--- image_resizing/image_resizing_consumer.py
+++ image_resizing/image_resizing_consumer.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python
 import pika, sys, os
+import subprocess
 
 def main():
+    # Stablish a connection to the Rabbit Queue
     credential = pika.PlainCredentials('rodrigo', 'rodrigo123')
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='image_resizing_rest_api', credentials=credential))
     channel = connection.channel()
-
     channel.queue_declare(queue='image_queue')
 
+    # Callback function is responsible for treating income messages and call 'convert' to resize images
     def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+        print("Starting resizing of image " + body.decode())
+        image_resize = subprocess.run(['convert', "-resize", "365X365!", "/stage/" + body.decode(), "/stage/resized/" + body.decode()])
+        print("Resizing of image " + body.decode() + " completed with return code ", image_resize.returncode)
 
     channel.basic_consume(queue='image_queue', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    # Message displayed when worker starts up
+    print("Waiting for messages. To exit press CTRL+C...")
+    
+    # A never-ending loop that waits for data and runs callbacks whenever necessary
     channel.start_consuming()
 
+# Catch KeyboardInterrupt during program shutdown
 if __name__ == '__main__':
     try:
         main()
--- rest_api/rest_api.py
+++ rest_api/rest_api.py
@@ -6,23 +6,21 @@ from app import app
 from flask import Flask, request, redirect, jsonify
 from werkzeug.utils import secure_filename
 
+# File extesion accepted as valid for API
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+# Port used to run Flask API
 PORT = 5000;
 
+# Function to check if provided file extesion is valid
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Define Flask API route for POST. /file-upload will be the endpoint
 @app.route('/file-upload', methods=['POST'])
 def upload_file():
 
-    # Open connection
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-
-    channel.queue_declare(queue='image_queue')
-
-    # check if the post request has the file part
+    # Check if the post request has the file part, return status code 400 if NOT
     if 'file' not in request.files:
         resp = jsonify({'message' : 'No file part in the request'})
         resp.status_code = 400
@@ -32,10 +30,23 @@ def upload_file():
         resp = jsonify({'message' : 'No file selected for uploading'})
         resp.status_code = 400
         return resp
+    
+    # If provided file is valid, save it to /stage folder and publish a message on the queue. 
+    # Return code 201 if success
     if file and allowed_file(file.filename):
+
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # Open connection and publish a message with filename on the queue
+        # RabbitMQ server is on this container and is named image_queue
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+        channel.queue_declare(queue='image_queue')
         channel.basic_publish(exchange='', routing_key='image_queue', body=filename)
+        connection.close()
+
         resp = jsonify({'message' : 'File successfully uploaded'})
         resp.status_code = 201
         return resp
@@ -43,8 +54,6 @@ def upload_file():
         resp = jsonify({'message' : 'Allowed file types are png, jpg, jpeg, gif'})
         resp.status_code = 400
         return resp
-
-    connection.close()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=PORT)
